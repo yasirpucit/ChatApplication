0a6a039a03fb131a0f0671a33b17784c5b95b811
yasirpucit <59232790+yasirpucit@users.noreply.github.com>
2020-03-13 21:59:13 +0500

    Chatting

---
+++ app/models/room.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const roomSchema = new Schema({
+  name1: { type: String, default: '', required: true },
+  name2: { type: String, default: '', required: true },
+  lastActive: { type: Date, default: Date.now },
+  createdOn: { type: Date, default: Date.now }
+});
+
+mongoose.model('Room', roomSchema);
---
+++ app/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  username: { type: String, default: '', required: true },
+  email: { type: String, default: '', required: true },
+  password: { type: String, default: '', required: true },
+  createdOn: { type: Date, default: Date.now },
+  updatedOn: { type: Date, default: Date.now }
+});
+mongoose.model('User', userSchema);
